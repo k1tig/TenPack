48bec676d9d5a00da1fdf3d470ebf34e63316fb9
Max Holden <m603holden@gmail.com>
2025-07-01 10:32:01 -0400

    rolled back to just message handling. ping push wasn't letting messages through

--- WS/main.go
+++ WS/main.go
@@ -3,20 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-var timeoutTimer time.Duration
-
 func main() {
-	chanMsg := make(chan struct{})
-	chanExit := make(chan bool)
-	timeoutTimer = 10 * time.Minute
-
-	defer close(chanExit)
-	defer close(chanMsg)
+	done := make(chan struct{})
 	dialer := websocket.Dialer{}
 	conn, _, err := dialer.Dial("ws://192.168.68.85:60003/velocidrone", nil) //check for static ip
 	if err != nil {
@@ -25,54 +17,29 @@ func main() {
 		log.Println("Connected to VD")
 	}
 	defer conn.Close()
-	keepAlive(conn, timeoutTimer, chanMsg, chanExit)
-outterloop:
+	go msgHandler(done, conn)
+	select {
+	case <-done:
+		return
+	}
+
+}
+
+func msgHandler(done chan struct{}, conn *websocket.Conn) {
 	for {
-		switch {
-		case <-chanExit:
-			conn.Close()
-			fmt.Print("closed")
-			break outterloop
+		select {
+		case <-done:
+			return
 		default:
 			_, message, err := conn.ReadMessage()
 			if err != nil {
-				log.Println("read:", err)
+				fmt.Println("Conn not available:", err)
+				close(done)
 				return
 			}
 			fmt.Println(string(message))
-			chanMsg <- struct{}{}
 		}
 	}
-	fmt.Println("\nExiting Program")
-	time.Sleep(3 * time.Second)
-}
-func keepAlive(c *websocket.Conn, timeout time.Duration, msg chan struct{}, chExit chan bool) {
-	LastResponse := time.Now()
-	c.SetPongHandler(func(msg string) error {
-		LastResponse = time.Now()
-		return nil
-	})
-	go func() {
-		for {
-			select {
-			case <-msg:
-				LastResponse = time.Now()
-			default:
-				err := c.WriteMessage(websocket.PingMessage, []byte("keepalive"))
-				if err != nil {
-					fmt.Println("Error Keepalive:", err)
-					return
-				}
-				time.Sleep(timeout / 2)
-				fmt.Println(LastResponse)
-				if time.Since(LastResponse) > timeout {
-					fmt.Print("\nInnactive Client timeout: Closing connection....")
-					chExit <- true
-					return
-				}
-			}
-		}
-	}()
 }
 
 /*
